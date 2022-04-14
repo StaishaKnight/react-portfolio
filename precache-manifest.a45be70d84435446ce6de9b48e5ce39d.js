c993bf7295a03013a98b5f11f2f499053387b840
StaishaKnight <staishaknight@gmail.com>
2022-04-14 15:14:16 -0500

    Updates

---
+++ precache-manifest.a45be70d84435446ce6de9b48e5ce39d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42bbddfa45aa72fda543cc180d22c01c",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "b6784d35050317e96e47",
+    "url": "/react-portfolio/static/css/main.5fb93635.chunk.css"
+  },
+  {
+    "revision": "ea3e331426f8aec8aaac",
+    "url": "/react-portfolio/static/js/2.bb1493c0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-portfolio/static/js/2.bb1493c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6784d35050317e96e47",
+    "url": "/react-portfolio/static/js/main.2db58340.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "1451e20f9d560d2d005b080bd9dc04e2",
+    "url": "/react-portfolio/static/media/LINK.1451e20f.jpg"
+  },
+  {
+    "revision": "0ec78018049c0ba2322bd4bebaf0ab5c",
+    "url": "/react-portfolio/static/media/resume.0ec78018.jpg"
+  }
+]);
